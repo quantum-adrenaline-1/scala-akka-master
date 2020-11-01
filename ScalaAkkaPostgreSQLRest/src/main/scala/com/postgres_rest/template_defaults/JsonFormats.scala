d9d29c9655336034652f5f30c14eb925f1343297
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2020-11-02 02:02:07 +0530

    Updated the type annotations for increasing performance, imported ActionPerformed and spray.json.RootJsonFormat in JsonFormats.scala

--- ScalaAkkaPostgreSQLRest/src/main/scala/com/postgres_rest/template_defaults/JsonFormats.scala
+++ ScalaAkkaPostgreSQLRest/src/main/scala/com/postgres_rest/template_defaults/JsonFormats.scala
@@ -1,14 +1,15 @@
 package com.postgres_rest.template_defaults
 
-import spray.json.DefaultJsonProtocol
+import com.postgres_rest.template_defaults.UserRegistry.ActionPerformed
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 object JsonFormats {
   // import the default encoders for primitive types (Int, String, Lists etc)
 
   import DefaultJsonProtocol._
 
-  implicit val userJsonFormat = jsonFormat3(User)
-  implicit val usersJsonFormat = jsonFormat1(Users)
+  implicit val userJsonFormat: RootJsonFormat[User] = jsonFormat3(User)
+  implicit val usersJsonFormat: RootJsonFormat[Users] = jsonFormat1(Users)
 
-  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+  implicit val actionPerformedJsonFormat: RootJsonFormat[ActionPerformed] = jsonFormat1(ActionPerformed)
 }
