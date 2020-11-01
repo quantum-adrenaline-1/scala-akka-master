422a873df0b8581844dd9ffcf0c56233bd3cb71f
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2020-11-02 01:27:56 +0530

    Renamed base package to a valid package name and moved the application template defaults to the package 'template_defaults'

--- ScalaAkkaPostgreSQLRest/src/test/scala/com/example/UserRoutesSpec.scala
+++ ScalaAkkaPostgreSQLRest/src/test/scala/com/example/UserRoutesSpec.scala
@@ -6,8 +6,9 @@ import akka.actor.testkit.typed.scaladsl.ActorTestKit
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import com.postgres_rest.template_defaults.{User, UserRegistry, UserRoutes}
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{ Matchers, WordSpec }
+import org.scalatest.{Matchers, WordSpec}
 
 //#set-up
 class UserRoutesSpec extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
@@ -29,7 +30,7 @@ class UserRoutesSpec extends WordSpec with Matchers with ScalaFutures with Scala
 
   // use the json formats to marshal and unmarshall objects in the test
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-  import JsonFormats._
+  import com.postgres_rest.template_defaults.JsonFormats._
   //#set-up
 
   //#actual-test
